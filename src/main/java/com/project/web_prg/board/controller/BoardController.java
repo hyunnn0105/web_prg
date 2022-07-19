3af53b482facc805cf4edfca45f20db1c4814725
hyunnn0105 <asha010519@gmail.com>
2022-07-19 11:36:22 +0900

    [220719] 쿠키생성 + @Transactional + HttpServletResponse/req

--- src/main/java/com/project/web_prg/board/controller/BoardController.java
+++ src/main/java/com/project/web_prg/board/controller/BoardController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -29,9 +31,9 @@ public class BoardController {
     }
 
     @GetMapping("/content/{boardNo}")
-    public String content(@PathVariable Long boardNo, Model model){
+    public String content(@PathVariable Long boardNo, Model model, HttpServletResponse response, HttpServletRequest request){
         log.info("controller request /board/list GET");
-        Board board = boardService.findOneService(boardNo);
+        Board board = boardService.findOneService(boardNo, response,request);
         log.info("return data - {}", board);
         model.addAttribute("b", board);
         return "board/board-detail";
@@ -40,7 +42,7 @@ public class BoardController {
     // 게시물 쓰기 화면 요청
     @GetMapping("/write")
     public String write(){
-        log.info("controller req /board/write GET");
+        log.info("controller request /board/write GET");
         return "board/board-write";
     }
 
@@ -48,7 +50,7 @@ public class BoardController {
     @PostMapping("/write")
 //    public String write(@RequestBody Board board){ <- test용
     public String write( Board board){
-        log.info("controller req /board/write POST! - {}", board);
+        log.info("controller request /board/write POST! - {}", board);
         // db 넣기
         boolean flag = boardService.saveService(board);
         return flag ? "redirect:/board/list" : "redirect:/";
@@ -57,17 +59,17 @@ public class BoardController {
     // 게시물 삭제 요청
     @GetMapping("/delete")
     public String delete(Long boardNo){
-        log.info("controller req /board/delete GET - {}", boardNo);
+        log.info("controller request /board/delete GET - {}", boardNo);
         return boardService.removeService(boardNo) ? "redirect:/board/list" : "redirect:/";
     }
 
     // 수정화면 요청 GET --> overloding 규칙 위반 (안에 들어가는 값이 달라야한다)
+    // findOne 있어서 req/rep 생성해서 넘김
     @GetMapping("/modify")
-    public String modify(Long boardNo, Model model){
-        log.info("controller req /board/modify GET! - bno {}", boardNo);
-        Board board = boardService.findOneService(boardNo);
+    public String modify(Long boardNo, Model model, HttpServletRequest request, HttpServletResponse response){
+        log.info("controller request /board/modify GET! - bno {}", boardNo);
+        Board board = boardService.findOneService(boardNo, response ,request);
         model.addAttribute("board", board);
-//        return boardService.modifyService(board) ? "redirect:/board/list" :"redirect:/";
         return "board/board-modify";
     }
 
@@ -75,7 +77,7 @@ public class BoardController {
     // 수정 처리 요청 POST-> ㅠㅏ라미터 방식으로 값 넘기기 X
     @PostMapping("/modify")
     public String modify(Board board){
-        log.info("controller req /board/modify POST - {}", board);
+        log.info("controller request /board/modify POST - {}", board);
         boolean flag = boardService.modifyService(board);
         return flag ? "redirect:/board/content/" + board.getBoardNo() : "redirect:/";
     }
--- src/main/java/com/project/web_prg/board/service/BoardService.java
+++ src/main/java/com/project/web_prg/board/service/BoardService.java
@@ -5,7 +5,12 @@ import com.project.web_prg.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.WebUtils;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -76,9 +81,36 @@ public class BoardService {
         }
     }
 
-    public Board findOneService(Long boardNo){
+    @Transactional // 전부 성공해야 한다        응답객체? -> 응답할떄 쿠키 전송
+    public Board findOneService(Long boardNo, HttpServletResponse response, HttpServletRequest request){
         log.info("findOne ser start- {}", boardNo);
-        return repository.findOne(boardNo);
+        Board board = repository.findOne(boardNo);
+        
+        // 해당 게시물 번호에 해당하는 쿠키가 있는지 확인
+        // 1. 쿠키가 없으면 조회수를 상승시켜주고 2.쿠키를 만들어서 클라이언트에 전송
+
+
+        makeViewCount(boardNo, response, request);
+
+
+        return board;
+    }
+
+    private void makeViewCount(Long boardNo, HttpServletResponse response, HttpServletRequest request) {
+        // 1. 해당 이름의 쿠키가 있으면 쿠키가 들어오고 없ㅇ,면 null이 들어옴
+        Cookie foundCookie = WebUtils.getCookie(request, "b" + boardNo);
+
+
+        // 2.
+        if (foundCookie == null) {
+            repository.upViewCount(boardNo);
+
+            Cookie cookie = new Cookie("b" + boardNo, String.valueOf(boardNo));// 쿠키생성
+            cookie.setMaxAge(60); // 쿠키 수명 설정(초단위) 60 * 60 이런식으로 설정 가능
+            cookie.setPath("/board/content"); // cookie 작동 범위->컨텐트 아래 전부
+
+            response.addCookie(cookie); //클라이언트에 쿠키 전송
+        }
     }
 
     // 게시물 삭제 요청 중간 처리
@@ -90,6 +122,7 @@ public class BoardService {
     // 게시물 수정 요청 중간 처리
     public boolean modifyService(Board board) {
         log.info("modify service start - {}", board);
+//        makeViewCount(board.getBoardNo(),response, request);
         return repository.modify(board);
     }
 
